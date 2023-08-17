142e5169dad835ac15aaf915b356ab769d1f9a49
Will Howard <hi@willhoward.co.uk>
2023-08-17 17:06:06 +0100

    Update Button styling.

--- src/components/Theme/Theme.tsx
+++ src/components/Theme/Theme.tsx
@@ -3,6 +3,11 @@ import { ThemeContextType, ThemeProviderProps } from "./Theme.types";
 
 const defaultTheme = {
   black: "#000000",
+  darkestGray: "#121212",
+  darkGray: "#232323",
+  lightGray: "#313030",
+  lightestGray: "#A7A7A7",
+  white: "#FFFFFF",
 };
 
 const ThemeContext = createContext<ThemeContextType | null>(null);
--- src/components/Theme/Theme.types.ts
+++ src/components/Theme/Theme.types.ts
@@ -2,6 +2,11 @@ import { ReactNode } from "react";
 
 export interface ThemeContextType {
   black: string;
+  darkestGray: string;
+  darkGray: string;
+  lightGray: string;
+  lightestGray: string;
+  white: string;
 }
 
 export interface ThemeProviderProps {
